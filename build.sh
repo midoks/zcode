e21c53fc18311a71773c3d58dd918ce1d3d28ce8
midoks <midoks@163.com>
2021-12-25 18:58:41 +0800

    up

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#load all 
+softList=`ls /usr/local/Cellar/`
+for i in $softList; do
+	newest=`ls /usr/local/Cellar/$i |cut -d \  -f 1 | awk 'END {print}'`
+	pconfig=/usr/local/Cellar/$i/$newest/lib/pkgconfig
+	PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$pconfig
+done
+# echo $softList
+
+# export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/Cellar/gtk4/4.4.0/lib/pkgconfig
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/opt/X11/lib/pkgconfig
+
+# gcc -o hello hello.c `pkg-config --cflags --libs gtk4`
+
+#g4=`pkg-config --libs --cflags gtk4`
+#echo $g4
+
+
+clang `pkg-config --libs --cflags gtk4` -O3 -g0  -o hello hello.c
